492c5d0ae379ee464d06b3cc043427110e16a642
Bugra <om1xjni9b@mozmail.com>
2022-03-28 20:06:59 +0300

    Added Typescript declaration file (index.d.ts)

---
+++ src/index.d.ts
@@ -0,0 +1,69 @@
+interface FileInfo {
+    path: string;
+    size: number;
+}
+
+interface FilePath {
+    local: string;
+    real: string;
+}
+
+interface Dataset {
+    pointer: number;
+    path: string;
+    type: string;
+}
+
+interface DatasetList {
+    datasets: Array<Dataset>;
+    errors: Array<string>;
+}
+
+interface Layer {
+    name: string;
+    featureCount: number;
+}
+
+interface DatasetInfo {
+    type: string;
+    dsName: string;
+    driverName: string;
+    bandCount?: number;
+    width?: number;
+    height?: number;
+    projectionWkt?: string;
+    coordinateTransform?: Array<number>;
+    corners?: Array<Array<number>>;
+    layerCount?: number;
+    featureCount?: number;
+    layers?: Array<Layer>;
+}
+
+interface Drivers {
+    raster: Object;
+    vector: Object;
+}
+
+interface Gdal {
+    ogr2ogr(dataset: Dataset, options: Array<string>): Promise<FilePath>;
+    gdal_translate(dataset: Dataset, options: Array<string>): Promise<FilePath>;
+    gdal_rasterize(dataset: Dataset, options: Array<string>): Promise<FilePath>;
+    gdalwarp(dataset: Dataset, options: Array<string>): Promise<FilePath>;
+    gdaltransform(coords: Array<Array<number>>, options: Array<string>): Promise<Array<Array<number>>>;
+    open(fileOrFiles: FileList|File|Array<string>|string): Promise<DatasetList>;
+    close(dataset: Dataset): Promise<void>;
+    getInfo(dataset: Dataset): Promise<DatasetInfo>;
+    getOutputFiles(): Promise<Array<FileInfo>>;
+    getFileBytes(filePath: string|FilePath): Promise<Uint8Array>;
+    drivers: Drivers;
+}
+
+interface Config {
+    path?: string;
+    dest?: string;
+    useWorker?: boolean;
+}
+
+declare module 'gdal3.js' {
+    export default function initGdalJs(config?: Config): Promise<Gdal>;
+}
